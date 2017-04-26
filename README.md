a734288a98f4e192ac7d2d39efb699d5f4cb6da3
Fernando Nantes-Machado <fernandonantesmachadoytoledo@gmail.com>
2017-04-26 18:26:42 +0100

    Modulo creado

---
+++ README.md
@@ -0,0 +1,3 @@
+# Módulo triangle
+
+Este repo contiene el módulo triangle que requiere del módulo Shape.
---
+++ index.js
@@ -0,0 +1,12 @@
+let Shape = require('@alu0100897975/ull-shape-fernando');
+
+class Triangle extends Shape {
+    constructor(options) {
+      super(options)
+    }
+    area() {
+      return 0.5 * this.width * this.height;
+    }
+}
+Shape.Shapes.Triangle = Triangle;
+module.exports = Triangle;
